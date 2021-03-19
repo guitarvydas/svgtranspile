f9cd74fee7b26ad79e11f407e8d1b927db1284a0
Paul Tarvydas <guitarvydas@gmail.com>
2021-03-19 14:08:00 -0400

    identity grammar using glue
    wip: reduced grammar
    
    
    wip: attr
    
    
    wip: svgelement
    
    
    wip: rect and text
    
    
    genericAttribute
    
    
    x/y/width/height/fill attributes
    
    
    elements expanded
    
    
    identity grammar for SVG through glue tool

--- run.bash
+++ run.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo >_temp.js
+node ../glue/glue.js <svg2p.glue >_temp.js  
 cat svg2p1.js _temp.js svg2p2.js >svg2p.js
 node svg2p <test.svg >_out.svg
 
--- svg2p.js
+++ svg2p.js
@@ -8,7 +8,7 @@ function ohm_parse (grammar, text) {
 	return { parser: parser, cst: cst };
     } else {
 	console.log (parser.trace (text).toString ());
-	throw "Ohm matching failed";
+	throw "svg2p: Ohm matching failed";
     }
 }
 
@@ -21,6 +21,222 @@ function getNamedFile (fname) {
     }	
 }
 
+function addSemantics (sem) { 
+  sem.addOperation (
+'_glue', 
+{
+
+               htmlsvg : function (_ws,_docH,_htmlH,_bodyH,_elements,_bodyE,_htmlE) { 
+                          var ws = _ws._glue ().join ('');
+var docH = _docH._glue ();
+var htmlH = _htmlH._glue ();
+var bodyH = _bodyH._glue ();
+var elements = _elements._glue ().join ('');
+var bodyE = _bodyE._glue ();
+var htmlE = _htmlE._glue ();
+                          return `${ws}${docH}${htmlH}${bodyH}${elements}${bodyE}${htmlE}`; 
+                        },
+            
+               htmlHeader : function (__,_ws) { 
+                          var _ = __._glue ();
+var ws = _ws._glue ().join ('');
+                          return `${_}${ws}`; 
+                        },
+            
+               htmlEnd : function (__,_ws) { 
+                          var _ = __._glue ();
+var ws = _ws._glue ().join ('');
+                          return `${_}${ws}`; 
+                        },
+            
+               bodyHeader : function (__,_ws) { 
+                          var _ = __._glue ();
+var ws = _ws._glue ().join ('');
+                          return `${_}${ws}`; 
+                        },
+            
+               bodyEnd : function (__,_ws) { 
+                          var _ = __._glue ();
+var ws = _ws._glue ().join ('');
+                          return `${_}${ws}`; 
+                        },
+            
+               docTypeHeader : function (__1,_stuff,__2,_ws) { 
+                          var _1 = __1._glue ();
+var stuff = _stuff._glue ().join ('');
+var _2 = __2._glue ();
+var ws = _ws._glue ().join ('');
+                          return `${_1}${stuff}${_2}${ws}`; 
+                        },
+            
+               element : function (_e) { 
+                          var e = _e._glue ();
+                          return `${e}`; 
+                        },
+            
+               svgElement : function (__1,__2,_ws,_attributes,__5,__6,_elements,_text,__9,__10,__11,__12) { 
+                          var _1 = __1._glue ();
+var _2 = __2._glue ();
+var ws = _ws._glue ().join ('');
+var attributes = _attributes._glue ().join ('');
+var _5 = __5._glue ();
+var _6 = __6._glue ().join ('');
+var elements = _elements._glue ().join ('');
+var text = _text._glue ().join ('');
+var _9 = __9._glue ();
+var _10 = __10._glue ();
+var _11 = __11._glue ();
+var _12 = __12._glue ().join ('');
+                          return `${_1}${_2}${ws}${attributes}${_5}${_6}${elements}${text}${_9}${_10}${_11}${_12}`; 
+                        },
+            
+               rectElement : function (__1,__2,_ws,_attributes,__5,__6,_elements,_text,__9,__10,__11,__12) { 
+                          var _1 = __1._glue ();
+var _2 = __2._glue ();
+var ws = _ws._glue ().join ('');
+var attributes = _attributes._glue ().join ('');
+var _5 = __5._glue ();
+var _6 = __6._glue ().join ('');
+var elements = _elements._glue ().join ('');
+var text = _text._glue ().join ('');
+var _9 = __9._glue ();
+var _10 = __10._glue ();
+var _11 = __11._glue ();
+var _12 = __12._glue ().join ('');
+                          return `${_1}${_2}${ws}${attributes}${_5}${_6}${elements}${text}${_9}${_10}${_11}${_12}`; 
+                        },
+            
+               textElement : function (__1,__2,_ws,_attributes,__5,__6,_elements,_text,__9,__10,__11,__12) { 
+                          var _1 = __1._glue ();
+var _2 = __2._glue ();
+var ws = _ws._glue ().join ('');
+var attributes = _attributes._glue ().join ('');
+var _5 = __5._glue ();
+var _6 = __6._glue ().join ('');
+var elements = _elements._glue ().join ('');
+var text = _text._glue ().join ('');
+var _9 = __9._glue ();
+var _10 = __10._glue ();
+var _11 = __11._glue ();
+var _12 = __12._glue ().join ('');
+                          return `${_1}${_2}${ws}${attributes}${_5}${_6}${elements}${text}${_9}${_10}${_11}${_12}`; 
+                        },
+            
+               basicElement : function (__1,__2,_ws,_attributes,__5,__6,_elements,_text,__9,__10,__11,__12) { 
+                          var _1 = __1._glue ();
+var _2 = __2._glue ();
+var ws = _ws._glue ().join ('');
+var attributes = _attributes._glue ().join ('');
+var _5 = __5._glue ();
+var _6 = __6._glue ().join ('');
+var elements = _elements._glue ().join ('');
+var text = _text._glue ().join ('');
+var _9 = __9._glue ();
+var _10 = __10._glue ();
+var _11 = __11._glue ();
+var _12 = __12._glue ().join ('');
+                          return `${_1}${_2}${ws}${attributes}${_5}${_6}${elements}${text}${_9}${_10}${_11}${_12}`; 
+                        },
+            
+               attribute : function (_a) { 
+                          var a = _a._glue ();
+                          return `${a}`; 
+                        },
+            
+               widthAttribute : function (__,__eq,_str,__ws) { 
+                          var _ = __._glue ();
+var _eq = __eq._glue ();
+var str = _str._glue ();
+var _ws = __ws._glue ().join ('');
+                          return `${_}${_eq}${str}${_ws}`; 
+                        },
+            
+               heightAttribute : function (__,__eq,_str,__ws) { 
+                          var _ = __._glue ();
+var _eq = __eq._glue ();
+var str = _str._glue ();
+var _ws = __ws._glue ().join ('');
+                          return `${_}${_eq}${str}${_ws}`; 
+                        },
+            
+               xAttribute : function (__,__eq,_str,__ws) { 
+                          var _ = __._glue ();
+var _eq = __eq._glue ();
+var str = _str._glue ();
+var _ws = __ws._glue ().join ('');
+                          return `${_}${_eq}${str}${_ws}`; 
+                        },
+            
+               yAttribute : function (__,__eq,_str,__ws) { 
+                          var _ = __._glue ();
+var _eq = __eq._glue ();
+var str = _str._glue ();
+var _ws = __ws._glue ().join ('');
+                          return `${_}${_eq}${str}${_ws}`; 
+                        },
+            
+               fillAttribute : function (__,__eq,_str,__ws) { 
+                          var _ = __._glue ();
+var _eq = __eq._glue ();
+var str = _str._glue ();
+var _ws = __ws._glue ().join ('');
+                          return `${_}${_eq}${str}${_ws}`; 
+                        },
+            
+               genericAttribute : function (__,__eq,_str,__ws) { 
+                          var _ = __._glue ();
+var _eq = __eq._glue ();
+var str = _str._glue ();
+var _ws = __ws._glue ().join ('');
+                          return `${_}${_eq}${str}${_ws}`; 
+                        },
+            
+               text : function (_x) { 
+                          var x = _x._glue ();
+                          return `${x}`; 
+                        },
+            
+               name : function (_c,_cs) { 
+                          var c = _c._glue ();
+var cs = _cs._glue ().join ('');
+                          return `${c}${cs}`; 
+                        },
+            
+               name1st : function (_c) { 
+                          var c = _c._glue ();
+                          return `${c}`; 
+                        },
+            
+               nameFollow : function (_c) { 
+                          var c = _c._glue ();
+                          return `${c}`; 
+                        },
+            
+               stuff : function (_c) { 
+                          var c = _c._glue ();
+                          return `${c}`; 
+                        },
+            
+               string : function (__1,_cs,__2) { 
+                          var _1 = __1._glue ();
+var cs = _cs._glue ().join ('');
+var _2 = __2._glue ();
+                          return `"${cs}"`; 
+                        },
+            
+               notQ : function (_c) { 
+                          var c = _c._glue ();
+                          return `${c}`; 
+                        },
+            
+               ws : function (_c) { 
+                          var c = _c._glue ();
+                          return `${c}`; 
+                        },
+            
+_terminal: function () { return this.primitiveValue; }
+}); 
+}
 
 
 
@@ -34,13 +250,13 @@ function main () {
     var sem = {};
     var outputString = "";
     if (cst.succeeded ()) {
-	// sem = parser.createSemantics ();
-	// addSemantics (sem);
-	// outputString = sem (cst).svg2p ();
+	sem = parser.createSemantics ();
+	addSemantics (sem);
+	outputString = sem (cst)._glue ();
     }
     return { cst: cst, semantics: sem, resultString: outputString };
 }
 
 
 var { cst, semantics, resultString } = main ();
-console.log(resultString);
+process.stdout.write(resultString);
